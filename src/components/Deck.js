34a63c4c36c6eb27799dd7fc092cb74af19447ce
Billy Dean <billy.goehring@gmail.com>
2023-03-27 13:25:22 -0700

    Set target positions for chosen cards from deck

--- src/components/Deck.js
+++ src/components/Deck.js
@@ -1,15 +1,15 @@
+// eslint-disable-next-line;
 import Card from './Card';
-
 function Deck () {
     const cards = [1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23];
 
 
 
-    return ( //eslint-disable-line
+    return ( 
 
         <ul className="deck">
             { cards.map((entry, index)=> {
-                return <li key={index} className={`card deck-pos-${entry}`}><Card /></li>
+                return <li key={index} className={`card deck-pos-${entry}`}> <Card /> </li>
             }) }
         </ul>
     )
