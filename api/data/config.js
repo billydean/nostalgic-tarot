99c208aefb905f9a9dd716f56ecb277bf5b926c4
Billy Dean <billy.goehring@gmail.com>
2023-06-12 13:04:24 -0700

    moving a lot of things around to make vercel happy. fingers crossed

--- api/data/config.js
+++ api/data/config.js
@@ -1,5 +1,5 @@
 const knex = require('knex');
 
-const configurations = require('../../knexfile');
+const configurations = require('../../knexfile.js');
 
 module.exports = knex(configurations)
--- knexfile.js
+++ knexfile.js
@@ -2,14 +2,14 @@ module.exports = {
     client: 'sqlite3',
     useNullAsDefault: true,
     migrations: {
-        directory: './src/data/migrations'
+        directory: './api/data/migrations'
     },
     seeds: {
-        directory: './src/data/seeds'
+        directory: './api/data/migrations'
     },
     pool: {
         afterCreate: (conn, done) => conn.run('PRAGMA foreign_keys = ON', done)},
     connection: {
-        filename: './src/data/cards.db3'
+        filename: './api/data/cards.db3'
     }
     };
