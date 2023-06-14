685d4b6e1c85949541b8ead73b5ee1d693c84d80
billydean <billy.goehring@gmail.com>
2023-06-14 15:29:18 -0700

    dots and dashes

--- src/app/components/Reading.jsx
+++ src/app/components/Reading.jsx
@@ -9,31 +9,31 @@ function ReadingSpace (props) {
     return (
         <div className={props.class}>
            { props.started && <div className="reading-content">
-                <div className="reading-1">
+                <div className="reading-1 read-left">
                     <h3>Present Situation</h3>
                     <p>Where do things currently stand? What circumstances led you to seek a reading?</p>
                     <h4>{cards[0].title}</h4>
                     <p>{cards[0].reading}</p>
                 </div>
-                <div className="reading-2">
+                <div className="reading-2 read-right">
                     <h3>Influences</h3>
                     <p>What factors or actors have their finger on the scale? What is contributing to the situation?</p>
                     <h4>{cards[1].title}</h4>
                     <p>{cards[1].reading}</p>
                 </div>
-                <div className="reading-3">
+                <div className="reading-3 read-left">
                     <h3>Challenges</h3>
                     <p>What stands in the way of resolving the situation?</p>
                     <h4>{cards[2].title}</h4>
                     <p>{cards[2].reading}</p>
                 </div>
-                <div className="reading-4">
+                <div className="reading-4 read-right">
                     <h3>Final Outcome</h3>
                     <p>If nothing else changes, what is most likely to happen?</p>
                     <h4>{cards[3].title}</h4>
                     <p>{cards[3].reading}</p>
                 </div>
-                <div className="reading-5">
+                <div className="reading-5 read-left">
                     <h3>Theme</h3>
                     <p>This is the lens through which all the other cards should be read.</p>
                     <h4>{cards[4].title}</h4>
