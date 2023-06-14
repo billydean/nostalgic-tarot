3518dae6df778462166756d3c33208df1a1c0f14
billydean <billy.goehring@gmail.com>
2023-06-14 13:53:30 -0700

    nav work

--- src/app/components/Header.jsx
+++ src/app/components/Header.jsx
@@ -6,8 +6,11 @@ function Header () {
 
     return (
         <header className="header">
-            <h1>Nostalgic Video Game Tarot</h1>
-            <p> A master of gift-giving, my brother created a custom series of major arcana cards featuring highlights from games we played growing up. They were too good to keep to myself. For a fairly simple tarot reading, <i>select five cards</i> from the deck below and<i> press &quot;Start.&quot;</i></p>
+            <div className="header-text">
+                <h1>Nostalgic Video Game Tarot</h1>
+                <p> A master of gift-giving, my brother created a custom series of major arcana cards featuring highlights from games we played growing up. They were too good to keep to myself. For a fairly simple tarot reading, <i>select five cards</i> from the deck below and<i> press &quot;Start.&quot;</i>
+                </p>
+            </div>
             <Nav />
         </header>
     )
--- src/app/components/Nav.jsx
+++ src/app/components/Nav.jsx
@@ -3,13 +3,11 @@ import React from "react";
 function Nav () {
 
     return (
-        <nav className="navbar">
-            <ul>
-                <li><a href="https://github.com/billydean">Github</a></li>
-                <li><a href="https://www.linkedin.com/in/billy-dean-goehring/">LinkedIn</a></li>
-                <li><a href="https://docs.google.com/document/d/1cpYZsiPlNL8ZGjxYN2Ktq-PVGW7NkIAxUWfvMXtSCBE/edit?usp=sharing">Resume</a></li>
+            <ul className="navbar">
+                <li><a href="https://github.com/billydean"><b>g</b>ithub</a></li>
+                <li><a href="https://www.linkedin.com/in/billy-dean-goehring/"><b>l</b>inkedin</a></li>
+                <li><a href="https://docs.google.com/document/d/1cpYZsiPlNL8ZGjxYN2Ktq-PVGW7NkIAxUWfvMXtSCBE/edit?usp=sharing"><b>r</b>esume</a></li>
             </ul>
-        </nav>
     )
 }
 
