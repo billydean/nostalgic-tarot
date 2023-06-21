ecc83c30a6054e4b174c32a48cd09385b7b90fc8
billydean <billy.goehring@gmail.com>
2023-06-21 12:53:12 -0700

    At least mostly functional in response to most screen sizes. A lot about the design philosphy doesn't work though. Accessible to devices, but perhaps not accessible for human beings.

--- src/app/components/Reading.jsx
+++ src/app/components/Reading.jsx
@@ -11,7 +11,7 @@ function ReadingSpace (props) {
            { props.started && <div className="reading-content">
                 <div className="reading-1">
                     <h3>Present Situation</h3>
-                    <p>Where do things currently stand? What circumstances led you to seek a reading?</p>
+                    <p className="h3-tag">Where do things currently stand? What circumstances led you to seek a reading?</p>
                     <h4>{cards[0].title}</h4>
                     <p>{cards[0].reading}</p>
                     <div className="read-back"></div>
@@ -19,7 +19,7 @@ function ReadingSpace (props) {
                 </div>
                 <div className="reading-2">
                     <h3>Influences</h3>
-                    <p>What factors or actors have their finger on the scale? What is contributing to the situation?</p>
+                    <p className="h3-tag">What factors or actors have their finger on the scale? What is contributing to the situation?</p>
                     <h4>{cards[1].title}</h4>
                     <p>{cards[1].reading}</p>
                     <div className="read-back"></div>
@@ -27,7 +27,7 @@ function ReadingSpace (props) {
                 </div>
                 <div className="reading-3">
                     <h3>Challenges</h3>
-                    <p>What stands in the way of resolving the situation?</p>
+                    <p className="h3-tag">What stands in the way of resolving the situation?</p>
                     <h4>{cards[2].title}</h4>
                     <p>{cards[2].reading}</p>
                     <div className="read-back"></div>
@@ -35,7 +35,7 @@ function ReadingSpace (props) {
                 </div>
                 <div className="reading-4">
                     <h3>Final Outcome</h3>
-                    <p>If nothing else changes, what is most likely to happen?</p>
+                    <p className="h3-tag">If nothing else changes, what is most likely to happen?</p>
                     <h4>{cards[3].title}</h4>
                     <p>{cards[3].reading}</p>
                     <div className="read-back"></div>
@@ -43,7 +43,7 @@ function ReadingSpace (props) {
                 </div>
                 <div className="reading-5">
                     <h3>Theme</h3>
-                    <p>This is the lens through which all the other cards should be read.</p>
+                    <p className="h3-tag">This is the lens through which all the other cards should be read.</p>
                     <h4>{cards[4].title}</h4>
                     <p>{cards[4].reading}</p>
                     <div className="read-back"></div>
